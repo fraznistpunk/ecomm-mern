4f60c901baf4ca2b451eb9099275d8746a47f3fc
Jatin_Kr <jackkr121@gmail.com>
2024-06-22 21:16:54 +0530

    define fnx for admin get all orders

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -83,7 +83,8 @@ const updateOrderToDelivered = asyncHandler(async (req, res) => {
 // @route GET /api/orders/:id/pay
 // @access PRIVATE/Admin
 const getOrders = asyncHandler(async (req, res) => {
-    res.send("Get all orders");
+    const orders = await Order.find({}).populate('user', 'id name');
+    res.status(200).json(orders);
 });
 
 export { addOrderItems, getMyOrders, getOrderById, updateOrderToPaid, updateOrderToDelivered, getOrders };
