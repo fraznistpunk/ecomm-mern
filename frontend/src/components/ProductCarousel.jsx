598329ee09ce28a225c213358f3665ec81b83215
Jatin Kumar <jackkr121@gmail.com>
2024-06-25 09:31:01 +0530

    Missing Message component import, fixed

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -3,10 +3,11 @@ import { CarouselItem, Carousel, Image } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { useGetTopProductsQuery } from '../slices/productsApiSlice';
 import Loader from "../components/Loader.jsx";
+import Message from "../components/Message.jsx";
 
 const ProductCarousel = () => {
     const { data : products, isLoading, error } = useGetTopProductsQuery();
-    return isLoading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
+    return isLoading ? <Loader /> : error ? <Message variant='danger'>{error.stack}</Message> : (
     <Carousel pause='hover' className='bg-primary mb-4'>
         {products.map(product => (
             <Carousel.Item key={product._id}>
