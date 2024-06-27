87aff2b2cc0c5511a4816edf03fa922ec1063ffc
Jatin_Kr <jackkr121@gmail.com>
2024-06-27 08:21:08 +0530

    Fixedd pagination bug

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,8 +5,8 @@ import Product from "../models/productModel.js";
 // @route GET /api/products/
 // @access PUBLIC
 const getProducts = asyncHandler(async (req, res) => {
-    const pageSize = 8;
-    const page = Number(req.params.pageNumber) || 1;
+    const pageSize = 1;
+    const page = Number(req.query.pageNumber) || 1;
 
     const keyword = req.query.keyword ? { name : { $regex : req.query.keyword, $options : 'i' }} : {};
     const count = await Product.countDocuments({ ...keyword });
