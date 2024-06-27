f20ae4dd1138623a63846eb68fad4a7933fd575a
Jatin_Kr <jackkr121@gmail.com>
2024-06-27 08:31:26 +0530

    Added missing value attribute to form control

--- frontend/src/components/SearchBox.jsx
+++ frontend/src/components/SearchBox.jsx
@@ -18,7 +18,7 @@ const SearchBox = () => {
     }
     return (
         <Form onSubmit={submitHandler} className='d-flex'>
-            <Form.Control type='text' name='q' onChange={(e) => setKeyword(e.target.value)} placeholder='Search products...' className='mr-sm-2 ml-sm-5'></Form.Control>
+            <Form.Control type='text' name='q' onChange={(e) => setKeyword(e.target.value)} value={keyword} placeholder='Search products...' className='mr-sm-2 ml-sm-5'></Form.Control>
             <Button type='submit' variant='outline-light' className='p-2 mx-2'>Search</Button>
         </Form>
     );
