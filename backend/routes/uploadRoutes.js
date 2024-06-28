11553763b765bbd754a1b946865ed93f0c10f407
Jatin_Kr <jackkr121@gmail.com>
2024-06-28 09:54:48 +0530

    Fixed upload routes and image validation on admin

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,37 +1,49 @@
 import path from "path";
 import express from "express";
 import multer from "multer";
+
 const router = express.Router();
 
 const storage = multer.diskStorage({
-    destination(req, file, callback) {
-        callback(null, 'uploads/'); // null is for error.
-    },
-    filename(req, file, callback) {
-        let newFileName = `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`;
-        callback(null, newFileName);
-    }
+  destination(req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename(req, file, cb) {
+    cb(
+      null,
+      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
 });
 
-function checkFileType(file, callback) {
-    const fileTypes = /jpg|jpeg|png/;
-    const extname = fileTypes.test(path.extname(file.originalname).toLowerCase());
-    const mimeType = filetypes.mimeType(file.mimetype);
-    if(extname && mimeType) {
-        return callback(null, true);
-    } else {
-        callback("Images only!");
-    }
-}
-const upload = multer({
-    storage,
-}); 
+function fileFilter(req, file, cb) {
+  const filetypes = /jpe?g|png|webp/;
+  const mimetypes = /image\/jpe?g|image\/png|image\/webp/;
 
-router.post('/', upload.single('image'), (req, res) => {
-    res.send({
-        message : "Image uploaded",
-        image : `/${req.file.path}`,
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = mimetypes.test(file.mimetype);
+
+  if (extname && mimetype) {
+    cb(null, true);
+  } else {
+    cb(new Error("Images only!"), false);
+  }
+}
+
+const upload = multer({ storage, fileFilter });
+const uploadSingleImage = upload.single("image");
+
+router.post("/", (req, res) => {
+  uploadSingleImage(req, res, function (err) {
+    if (err) {
+      return res.status(400).send({ message: err.message });
+    }
+
+    res.status(200).send({
+      message: "Image uploaded successfully",
+      image: `/${req.file.path}`,
     });
+  });
 });
 
 export default router;
